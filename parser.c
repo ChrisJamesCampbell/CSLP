6bca37572546173ce3825d6b046386404248dc2a
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-12-17 18:56:40 +0000

    Update parser.c
    Added floyd warshall alogithm code but struggling to test it. Will definitely need changing

--- parser.c
+++ parser.c
@@ -1,4 +1,4 @@
-///Prelimanary CSLP Simulator 
+//Prelimanary CSLP Simulator 
 
 
 #include <stdio.h>
@@ -58,7 +58,12 @@ struct request
   unsigned int from_stop;
   unsigned int to_stop;
   time_t for_departure;
-  time_t scheduled_for;
+
+  //think this is inappropriate to include in the request structure
+  //and should in fact be the calculated metric that's appended on in an output string
+  //at output stage
+  //time_t scheduled_for;
+
 
 };
 
@@ -67,30 +72,26 @@ struct bus
   unsigned int no_passengers;
   unsigned int current_stop;
   unsigned int next_stop;
-  unsigned int path[]; //to hold an array of stops
+  unsigned int *path; //to hold an array of stops
 };
+
+
 
 
 static void initialise_input_file(struct input_file *test_input)
 {
-  int row, column = 0;
+  //int row, column = 0;
 
   test_input->busCapacity = 0;
-
   test_input->boardingTime = 0;
-
   test_input->requestRate = 0.0;
-
   test_input->pickupInterval = 0.0;
-
   test_input->maxDelay = 0;
-
   test_input->noBuses = 0;
-
   test_input->noStops = 0;
 
   //test_input->map[noStops][noStops] = [0][0];
-  for(row = 0; row < 6; row++)
+  /*for(row = 0; row < 6; row++)
   {
     for(column = 0; column < 6; column++)
     {
@@ -98,15 +99,29 @@ static void initialise_input_file(struct input_file *test_input)
     }
   }
 
+  */
+
   test_input->stopTime = 0;
   return;
 
 }
 
+static void initialise_fleet(struct input_file *test_input, struct bus fleet[test_input->noBuses])
+{
+  int i;
+  for(i = 0; i < test_input->noBuses; i++)
+  {
+    fleet[i].no_passengers = 0;
+    fleet[i].current_stop = 0;
+    fleet[i].next_stop = 0;
+    fleet[i].path = malloc(sizeof(int) * test_input->noStops); //temporary idea for allowed size of path
+  }
+}
 
 
 
-static void parse_input(struct input_file *test_input)
+
+static void parse_input(struct input_file *test_input, char* input)
 {
   FILE *fp;
   char line[200];
@@ -117,20 +132,10 @@ static void parse_input(struct input_file *test_input)
 
 
 
-  /*int rowIndex = test_input->noStops;
-  int colIndex = test_input->noStops;
 
-  for(rowIndex = 0; rowIndex < colIndex; rowIndex++)   
-      free(map[rowIndex]);
-    free(map);*/
 
-  /*map = (int **) malloc(colIndex * sizeof(int*));  // rows
-  for(rowIndex = 0; rowIndex < colIndex; rowIndex++)   
-    map[rowIndex] = (int *) malloc(colIndex * sizeof(int));  // columns
-*/
-
-  fp = fopen("/afs/inf.ed.ac.uk/user/s13/s1334028/Documents/CSLP/simulator_test_input", "r");
-
+  fp = fopen(input, "r");
+    
   
    while(1)
 
@@ -155,12 +160,12 @@ static void parse_input(struct input_file *test_input)
 
     if(strncmp("requestRate", line, 11) == 0)
     {
-      sscanf(line+11, "%*[ ]%lf", &test_input->requestRate);
+      sscanf(line+11, "%*[ ]%f", &test_input->requestRate);
     }
 
     if(strncmp("pickupInterval", line, 14) == 0)
     {
-      sscanf(line+14, "%*[ ]%lf", &test_input->pickupInterval);
+      sscanf(line+14, "%*[ ]%f", &test_input->pickupInterval);
     }
 
     if(strncmp("maxDelay", line, 8) == 0)
@@ -177,74 +182,44 @@ static void parse_input(struct input_file *test_input)
     {
       sscanf(line+7, "%*[ ]%d", &test_input->noStops);
     }
+
+  int rowIndex = test_input->noStops;
+  int colIndex = test_input->noStops;
+
+  
+  test_input->map = (int **) malloc(colIndex * sizeof(int*));  // rows
+  for(rowIndex = 0; rowIndex < colIndex; rowIndex++)  
+  { 
+    test_input->map[rowIndex] = (int *) malloc(colIndex * sizeof(int));
+  }  // columns
+
  
- /*
+  
   if(strncmp("map", line, 3) == 0)
    {
     fgets(line, 200, fp);
 
     tempRow = strtok(line, "\n");
-    printf("The value of tempRow is: %s", tempRow);
-    for(int row = 0; row < 6; row++)
+    //printf("The value of tempRow is: %s", tempRow);
+    for(int row = 0; row < test_input->noStops; row++)
     {
-      for(int column = 0; column < 6; column++)
+      token = strtok(tempRow, delim);
+      test_input->map[row][0] = atoi(token);
+
+      for(int column = 1; column < test_input->noStops; column++)
       {
-        printf("The value of token is: %s", token);
-        token = strtok(tempRow, delim);
+        //printf("The value of token is: %s", token);
         token = strtok(NULL, delim);
+        //token = strtok(NULL, delim);
         test_input->map[row][column] = atoi(token);
       }
 
       fgets(line, 200, fp); 
-    }
+      }
+    } //closes the end of map reading
     
-
-    
-    int bool = 1;
-    int tempInnerRow = 0;
-    
-      tempRow = strtok(line, "\n");
-      //printf("\nthe value of temp row was %s\n", tempRow);
-      
-
-      
-        while(tempInnerRow < 6)
-        {
-          for(int column = 0; column < 6; column++)
-          {
-            if (bool == 1){
-              token = strtok(line, delim);
-              //printf("Value of token: %s\n", token);
-              bool = 0;
-            } else {
-              token = strtok(NULL, delim);
-              //printf("The value of token was: %s\n", token);
-            }
-              
-            if(token != NULL)
-            {
-              test_input->map[tempInnerRow][column] = atoi(token);
-            } 
-            
-            else 
-            {
-              fgets(line, 200, fp);
-              tempInnerRow++;
-              bool = 1;
-            }
-          }
-
-          
-        }
-
-      
-
-    
-
-  } //closes the end of map reading
-  */
-
-   if(strncmp("stopTime", line, 8) == 0);
+  
+   if(strncmp("stopTime", line, 8) == 0)
    {
     sscanf(line+8, "%*[ ]%d", &test_input->stopTime);
    }
@@ -254,12 +229,6 @@ static void parse_input(struct input_file *test_input)
   } //end of while fgets loop
 
   fclose(fp);
-}
-
-static double generate_random()
-{
-  double random_number = rand();
-  return random_number;
 }
 
 static long random_at_most(long max) {
@@ -272,7 +241,7 @@ static long random_at_most(long max) {
 
   long x;
   do {
-   x = random();
+   x = rand();
   }
   // This is carefully written not to overflow
   while (num_rand - defect <= (unsigned long)x);
@@ -300,94 +269,206 @@ static struct request generate_request(struct input_file *test_input)
   //this needs to convert 
   new_request.for_departure = (int)(new_request.time_stamp) + (random_at_most(test_input->pickupInterval) * 60); //time_stamp is in sscs and we need pickupInterval in secs too
 
-  //holy fucking shit balls, this is where the best possible route algorithm is gonna need to kick in
-  //FUCK MY LIFE
-  //new_request.scheduled_for = 
   return new_request;
 }
 
 static void handle_request(struct request *new_request)
 {
+
+
   return;
 }
 
-static char output_request(struct request *new_request)
+
+static struct input_file floyd_warshall(struct input_file *test_input) //temporary return idea for new map
 {
-  char str[1024];
-  char a[50], b[50], c[50], d[50], e[50];
+
+  struct input_file updated_map; //map will be the 
+  initialise_input_file(&updated_map);
+
+  int N = test_input->noStops;
+  int rowIndex = N;
+  int colIndex = N;
+  int **dist;  // this will be the new matrix of distances
+
+  ////////////////////////////////////////////////////////////////
+  //POSSIBLY SUPERFLIOUS CODE????????????????????????????????????
+  
+  dist = (int **) malloc(colIndex * sizeof(int*));  // rows
+  for(rowIndex = 0; rowIndex < colIndex; rowIndex++)
+  {   
+    dist[rowIndex] = (int *) malloc(colIndex * sizeof(int));
+  }
+
+  rowIndex = 0;
+  colIndex = 0;
+  for(rowIndex = 0; rowIndex < N; rowIndex++)
+  {
+    for(colIndex = 0; colIndex < N; rowIndex++)
+    {
+      dist[rowIndex][colIndex] = test_input->map[rowIndex][colIndex];
+    }
+
+  }
+
+  /////////////////////////////////////////////////////////////////
+
+  int i, j, k;
+  char route[] = "";
+  // Input data into dist, where dist[i][j] is the distance from i to j.
+   
+     for ( k = 0; k < N; k++ )
+     {
+        for ( i = 0; i < N; i++ )
+        {
+           for ( j = 0; j < N; j++ )
+           {
+            if(dist[i][k] == -1 || dist[k][j] == -1)
+            {
+              //do nothing, since route does not exist
+            }
+
+              else if(dist[i][j] > (dist[i][k] + dist[k][j]) || dist[i][j] == -1)
+              {
+                dist[i][j] = (dist[i][k] + dist[k][j]);
+                //str_append(route, k);
+              }
+                            
+           }
+        }
+     }
+
+  rowIndex = 0;
+  colIndex = 0;
+  for(rowIndex = 0; rowIndex < N; rowIndex++)
+  {
+    for(colIndex = 0; colIndex < N; rowIndex++)
+    {
+      updated_map.map[rowIndex][colIndex] = dist[rowIndex][colIndex];
+    }
+
+  }
+
+  /*
+  procedure Path(u, v)
+   if next[u][v] = null then
+       return []
+   path = [u]
+   while u ≠ v
+       u ← next[u][v]
+       path.append(u)
+   return path
+ */
 
 
 
-  strcpy(str, sprintf(a, "%d", new_request->time_stamp)); //need to convert this time to a string
+
+
+  return updated_map;
+}
+
+
+/*
+static char* output_request(struct request *new_request)
+{
+  char str = malloc(sizeof(char)*1024);
+  char a[50], b[50], c[50], d[50];
+
+
+
+  strcpy(str, sprintf(a, "%d", (int)new_request->time_stamp)); //need to convert this time to a string
   strcat(str, "-> new request placed from stop");
   strcat(str, sprintf(b, "%d", new_request->from_stop)); //need to convert this time to a string
   strcat(str, "to stop");
   strcat(str, sprintf(c, "%d", new_request->to_stop));
   strcat(str, "for departure at");
-  strcat(str, sprintf(d, "%d", new_request->for_departure));
-  strcat(str, "scheduled for");
-  strcat(str, sprintf(e, "%d", new_request->scheduled_for));
+  strcat(str, sprintf(d, "%d", (int)new_request->for_departure));
+  //strcat(str, "scheduled for");
+  //strcat(str, sprintf(e, "%d", new_request->scheduled_for));
 
   return str;
 }
+*/
 
 
 
-
-int main()
+int main(int argc, char* argv[])
 {
   
   struct input_file test_input;
 
-  initialise_input_file(&test_input);
+  //initialise_input_file(&test_input);
+  parse_input(&test_input, argv[1]);
 
-  parse_input(&test_input);
+  //time_t sec;
+  //sec = time (NULL);
 
-  time_t sec;
-  sec = time (NULL);
+  struct bus fleet[test_input.noBuses];
+
+  initialise_fleet(&test_input, fleet);
 
   printf("The Bus Capacity given in the test file was: %d persons.\n", test_input.busCapacity);
   printf("The Boarding Time given in the test file was: %d seconds.\n", test_input.boardingTime);
-  printf("The Request Rate given in the test file was: %lf seconds.\n", test_input.requestRate);
-  printf("The Pick Up Interval given in the test file was: %lf minutes.\n", test_input.pickupInterval);
+  printf("The Request Rate given in the test file was: %f seconds.\n", test_input.requestRate);
+  printf("The Pick Up Interval given in the test file was: %f minutes.\n", test_input.pickupInterval);
   printf("The Max Delay given in the test file was: %d minutes.\n", test_input.maxDelay);
   printf("The Numer of Buses given in the test file was: %d buses.\n", test_input.noBuses);
   printf("The Number of Stops in the test file was: %d stops.\n", test_input.noStops);
   printf("The Stop Time given in the test file was: %d seconds.\n", test_input.stopTime);
   printf("The map given in the test file was: \n");
 
-  for(int row = 0; row < 6; row++)
+
+  int row, column;
+  for(row = 0; row < test_input.noStops; row++)
   {
-    for(int column = 0; column < 6; column++)
+    for(column = 0; column < test_input.noStops; column++)
     {
 
       printf("%d ", test_input.map[row][column]);
-      if(column == 5)
+      if(column == test_input.noStops - 1)
       {
         printf("\n");
       }
     }
   }
 
-  
+ /* struct input_file fwdmap = floyd_warshall(&test_input);
 
+
+  printf("The Result of the Floyd-Warshall'd map is: \n");
+
+  for(row = 0; row < test_input.noStops; row++)
+  {
+    for(column = 0; column < test_input.noStops; column++)
+    {
+
+      printf("%d ", fwdmap.map[row][column]);
+      if(column == test_input.noStops - 1)
+      {
+        printf("\n");
+      }
+    }
+  }*/
+
+
+/*
 
   //loop for generating requests
   while(1)
   {
-    struct request new_request = generate_request(); //make a new quest
+    struct request new_request = generate_request(&test_input); //make a new quest
 
-    handle_request(struct request *new_request); //handle that same request
+    handle_request(&new_request); //handle that same request
 
-    printf("%s", output_request(struct request *new_request)); //output the outcome of handling that request to the terminal
+    //printf("%s", output_request(&new_request)); //output the outcome of handling that request to the terminal
 
     //needs to be uniformly distributed or some shit
    //sleep((int)generate_random());
-  sleep(5) //wait 5 seconds before creating the next request
+  sleep(5); //wait 5 seconds before creating the next request
 
   }
 
-
+*/
   return 0;
 
 }
